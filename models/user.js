581f9fce6649fa0013c4323e03e9d3e83f48f93e
Corey Howell <cjthowell@gmail.com>
2015-06-10 11:18:58 -0400

    Updated the user model to include firstName, lastName, and username. UI does not yet require these fields.

--- models/user.js
+++ models/user.js
@@ -4,6 +4,9 @@ var Mongoose = require('../database').Mongoose;
 var userSchema = new Mongoose.Schema({
 	email: 	      { type: String,	required: true },
 	password:     { type: String,	required: true },
+	firstName:    { type: String,	required: true },
+	lastName:     { type: String,	required: true },
+	username:     { type: String,	required: true },
 	creationDate: { type: Date,		required: true, default: Date.now },
 });
 
